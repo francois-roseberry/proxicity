2c8c884bad76341f6e6bebcb225bd7cf0f4e5e1d
froseberry <f_roseberry@live.fr>
2016-08-24 20:34:10 -0400

    First tests with nodejs

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var app = express();
+var homes = require('./src/fake-homes.js').homes;
 
 // Arg 0 will be node, arg 1 will be the name of this file, arg 2 will be the directory to serve
 var clientDirectory = process.argv[2] || '/';
 
 app.use(express.static(clientDirectory));
+
+app.get('/homes', function (request, response) {
+	response.send({homes: homes()});
+});
 
 var port = process.env.PORT || 3000;
 
---
+++ server/src/fake-homes.js
@@ -0,0 +1,7 @@
+exports.homes = function () {
+	return [
+		{name: 'House A'},
+		{name: 'Appartment B'},
+		{name: 'Condominium C'}
+	];
+};
