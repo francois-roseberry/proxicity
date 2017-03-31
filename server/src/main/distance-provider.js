d983e1aa8e0a97eaca0223d905ba9a65f474e513
francois-roseberry <f_roseberry@live.fr>
2017-03-31 14:20:27 -0400

    Used class declaration for DistanceProvider

--- server/src/main/distance-provider.js
+++ server/src/main/distance-provider.js
@@ -8,44 +8,46 @@ const precondition = require('./infrastructure/contract.js').precondition;
 const URL_TEMPLATE = 'https://maps.googleapis.com/maps/api/distancematrix/json' +
 	'?origins={0}&destinations={1}&mode=walking&key={2}';
 
-function DistanceProvider(provider, placeType, key) {
-	precondition(provider && _.isFunction(provider.getHomes), 'A DistanceProvider requires a provider');
-	precondition(_.isString(placeType), 'A DistanceProvider requires a place type');
-	precondition(_.isString(key), 'A DistanceProvider requires an API key');
-	
-	this._provider = provider;
-	this._placeType = placeType;
-	this._key = key;	
-}
+class DistanceProvider {
+	constructor(provider, placeType, key) {
+		precondition(provider && _.isFunction(provider.getHomes), 'A DistanceProvider requires a provider');
+		precondition(_.isString(placeType), 'A DistanceProvider requires a place type');
+		precondition(_.isString(key), 'A DistanceProvider requires an API key');
+		
+		this._provider = provider;
+		this._placeType = placeType;
+		this._key = key;	
+	}
 
-DistanceProvider.prototype.getHomes = function() {
-	var key = this._key;
-	var placeType = this._placeType;
-	var subject = new Rx.AsyncSubject();
-	this._provider.getHomes().subscribe(function (homes) {
-		var subjects = homes.map(function(home) {
-			var origin = encodeURIComponent(home.address);
-			var destination = encodeURIComponent(home[placeType].address);
-			var url	= URL_TEMPLATE
-				.replace('{0}', origin)
-				.replace('{1}', destination)
-				.replace('{2}', key);
+	getHomes() {
+		var key = this._key;
+		var placeType = this._placeType;
+		var subject = new Rx.AsyncSubject();
+		this._provider.getHomes().subscribe(function (homes) {
+			var subjects = homes.map(function(home) {
+				var origin = encodeURIComponent(home.address);
+				var destination = encodeURIComponent(home[placeType].address);
+				var url	= URL_TEMPLATE
+					.replace('{0}', origin)
+					.replace('{1}', destination)
+					.replace('{2}', key);
+				
+				return distanceSubject(url, home, placeType);
+			});
 			
-			return distanceSubject(url, home, placeType);
+			Rx.Observable.forkJoin(subjects).subscribe(function (homesWithGroceries) {
+				subject.onNext(homesWithGroceries);
+				subject.onCompleted();
+			});
 		});
 		
-		Rx.Observable.forkJoin(subjects).subscribe(function (homesWithGroceries) {
-			subject.onNext(homesWithGroceries);
-			subject.onCompleted();
-		});
-	});
-	
-	return subject.asObservable();
-};
+		return subject.asObservable();
+	}
+}
 
 function distanceSubject(url, home, placeType) {
 	var subject = new Rx.AsyncSubject();
-	request(url, function (error, response, body) {
+	request(url, (error, response, body) => {
 		if (error) {
 			subject.onError(error);
 		}
