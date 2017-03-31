d7e9673eaeee7921eff675ffb85ed73a07631766
francois-roseberry <f_roseberry@live.fr>
2017-03-31 14:07:06 -0400

    Used class declarations for CachedProvider

--- server/src/main/cached-provider.js
+++ server/src/main/cached-provider.js
@@ -5,34 +5,36 @@ const Rx = require('rx');
 const _ = require('underscore');
 const precondition = require('./infrastructure/contract').precondition;
 
-function CachedProvider(provider, filename) {
-	precondition(provider && _.isFunction(provider.getHomes), 'A CachedProvider requires a provider');
-	precondition(_.isString(filename), 'A CachedProvider requires a filename');
-	
-	this._provider = provider;
-	this._filename = filename;
-}
-
-CachedProvider.prototype.getHomes = function() {
-	var subject = new Rx.AsyncSubject();
-	var provider = this._provider;
-	var filename = this._filename;
-	fs.readFile(this._filename, 'utf-8', function (err, data) {
-		if (err) {
-			// Error reading, file's probably absent.
-			provider.getHomes().subscribe(cacheResults(filename, subject),
-				function (err) {
-					subject.onError(err);
-				});
-				
-			return;
-		}
+class CachedProvider {
+	constructor(provider, filename) {
+		precondition(provider && _.isFunction(provider.getHomes), 'A CachedProvider requires a provider');
+		precondition(_.isString(filename), 'A CachedProvider requires a filename');
 		
-		subject.onNext(JSON.parse(data));
-		subject.onCompleted();
-	});
-	return subject.asObservable();
-};
+		this._provider = provider;
+		this._filename = filename;
+	}
+
+	getHomes() {
+		var subject = new Rx.AsyncSubject();
+		var provider = this._provider;
+		var filename = this._filename;
+		fs.readFile(this._filename, 'utf-8', function (err, data) {
+			if (err) {
+				// Error reading, file's probably absent.
+				provider.getHomes().subscribe(cacheResults(filename, subject),
+					function (err) {
+						subject.onError(err);
+					});
+					
+				return;
+			}
+			
+			subject.onNext(JSON.parse(data));
+			subject.onCompleted();
+		});
+		return subject.asObservable();
+	}
+}
 
 function cacheResults(filename, subject) {
 	return function (homes) {
